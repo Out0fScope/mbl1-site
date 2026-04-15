95bc479cea573aa9e99eed614dc0fba6fb755692
Tsimafei Lipnitski <silencetray@gmail.com>
2026-04-15 14:15:19 +0300

    Changed footer title.

--- frontend/src/components/Footer/components/Info.tsx
+++ frontend/src/components/Footer/components/Info.tsx
@@ -4,7 +4,7 @@ import { IContact } from 'src/lib/api/types';
 const Info = ({ contacts }: { contacts: IContact }) => {
   return (
     <div className="flex flex-col gap-[0.5rem] max-w-xs">
-      <span className="text-4xl font-bold">MeBel1</span>
+      <span className="text-4xl font-bold">MeBeL1</span>
       <span className="text-muted text-sm mb-[0.5rem]">
         Отправляя формы на сайте, вы соглашаетесь на обработку персональных данных, защищенных{' '}
         <Link href="#" className="underline text-muted/50 hover:text-primary">
