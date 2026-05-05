4ad1202af5c6d802281be1c391293d2487b29e86
Tsimafei Lipnitski <silencetray@gmail.com>
2026-05-05 23:18:23 +0300

    some changes.

--- frontend/src/components/Header/components/Contacts.tsx
+++ frontend/src/components/Header/components/Contacts.tsx
@@ -84,7 +84,7 @@ const Contacts = ({ contacts }: Props) => {
                 hover:text-primary
               `}
             >
-              @mbl_1_mebel
+              @mbl_123_mebel
             </a>
           </div>
         </div>
