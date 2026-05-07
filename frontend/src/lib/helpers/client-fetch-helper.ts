35b7a80d15d6e1c8f29d4a719176ca91c0a0ccd7
Tsimafei Lipnitski <silencetray@gmail.com>
2026-05-07 20:05:06 +0300

    Fix order form + Directus integration + cleanup client API helpers

---
+++ frontend/src/lib/helpers/client-fetch-helper.ts
@@ -0,0 +1,40 @@
+const API_URL = process.env.NEXT_PUBLIC_CLIENT_API_URL;
+
+export const uploadFile = async (file: File) => {
+  const formData = new FormData();
+
+  formData.append('file', file);
+
+  const res = await fetch(`${API_URL}/files`, {
+    method: 'POST',
+    body: formData,
+  });
+
+  if (!res.ok) {
+    throw new Error('File upload failed');
+  }
+
+  const text = await res.text();
+
+  return text ? JSON.parse(text) : null;
+};
+
+export const createOrder = async (payload: any) => {
+  console.log(payload);
+
+  const res = await fetch(`${API_URL}/items/orders`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!res.ok) {
+    throw new Error('Order create failed');
+  }
+
+  const text = await res.text();
+
+  return text ? JSON.parse(text) : null;
+};
